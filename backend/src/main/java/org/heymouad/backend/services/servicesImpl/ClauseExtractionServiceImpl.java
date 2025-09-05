3e9022a9b1c8e716e450f677912340b324739bf2
devcom33 <devcom33@users.noreply.github.com>
2025-09-05 23:53:39 +0100

    create methode for extraction

--- backend/src/main/java/org/heymouad/backend/services/servicesImpl/ClauseExtractionServiceImpl.java
+++ backend/src/main/java/org/heymouad/backend/services/servicesImpl/ClauseExtractionServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
 import org.heymouad.backend.services.ClauseExtractionService;
+import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+@Service
 public class ClauseExtractionServiceImpl implements ClauseExtractionService {
     @Override
     public String extractText(Path filePath) throws IOException, TikaException, SAXException {
