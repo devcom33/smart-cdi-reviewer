4ed25c225be0801c7eb8a55b2a39fa2039066848
devcom33 <devcom33@users.noreply.github.com>
2025-09-28 20:18:19 +0100

    add new endpoint for polling

--- backend/src/main/java/org/heymouad/backend/controllers/ContactController.java
+++ backend/src/main/java/org/heymouad/backend/controllers/ContactController.java
@@ -1,9 +1,11 @@
 package org.heymouad.backend.controllers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.heymouad.backend.dtos.ContractResponse;
-import org.heymouad.backend.services.ContractReviewConsumerService;
 import org.heymouad.backend.services.ContractService;
 import org.heymouad.backend.services.servicesImpl.ContractReviewProducer;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,18 +15,20 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 @RequestMapping("/api/v1/")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ContactController {
     private final ContractService contractService;
     private final ContractReviewProducer contractReviewProducer;
     private final RedisTemplate<String, String> redisTemplate;
 
     @PostMapping(value = "/contracts/review", consumes = "multipart/form-data")
-    public ResponseEntity<String> reviewContract(@RequestParam("file") MultipartFile file) throws Exception
+    public ResponseEntity<Map<String, String>> reviewContract(@RequestParam("file") MultipartFile file) throws Exception
     {
         Set<String> allowedTypes = Set.of(
                 "application/pdf",
@@ -45,19 +49,38 @@ public class ContactController {
 
         String id = contractReviewProducer.sendToQueue(contractResponse);
 
-        return ResponseEntity.accepted().body("Contract uploaded. ID = " + id);
+        return ResponseEntity.accepted().body(Map.of("id", id));
     }
 
     @GetMapping("/result/{id}")
-    public ResponseEntity<String> getResult(@PathVariable String id) {
+    public ResponseEntity<?> getResult(@PathVariable String id) {
         String result = redisTemplate.opsForValue().get(id);
+        log.warn(" [+] Result : {}", result);
+
         if (result == null) {
-            return ResponseEntity.status(HttpStatus.PROCESSING)
-                    .body("Result not ready yet.");
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(Map.of("status", "processing", "message", "Result not ready yet."));
         }
-        return ResponseEntity.ok(result);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> resultMap = mapper.readValue(result, Map.class);
+            return ResponseEntity.ok(resultMap);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("status", "error", "message", "Result format error."));
+        }
     }
 
+    @PostConstruct
+    public void logRedisConnection() {
+        log.info("Redis connection factory: " + redisTemplate.getConnectionFactory().getConnection().toString());
+    }
+
+    @GetMapping("/debug/keys")
+    public ResponseEntity<?> debugKeys() {
+        Set<String> keys = redisTemplate.keys("*");
+        return ResponseEntity.ok(keys);
+    }
     @GetMapping("/contracts")
     public ResponseEntity<?> getAllContracts()
     {
