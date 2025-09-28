b517c0aa3f204e6c45efe892f51dcc1f055a435c
devcom33 <devcom33@users.noreply.github.com>
2025-09-28 20:17:52 +0100

    add redis

--- backend/src/main/java/org/heymouad/backend/config/RedisConfig.java
+++ backend/src/main/java/org/heymouad/backend/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -12,7 +13,8 @@ public class RedisConfig {
     {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
